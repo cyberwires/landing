64d008db6a190685a66e3a7170d6c76b5bfba07d
Volodymyr Prykhodko <diegomackenzie24@gmail.com>
2025-01-26 21:16:40 +0200

    deploy fix 2;

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,23 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = "";
+if (isGithubActions) {
+  const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+  repo = `/${repoName}`;
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  assetPrefix: "/landing",
+  basePath: repo || "",
+  assetPrefix: repo || "",
   images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     hostname: "localhost",
+    //   },
+    // ],
     unoptimized: true,
   },
 };
