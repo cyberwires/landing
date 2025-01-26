86ce13606038ff4d94a885f4ce084d9600b92bb7
Volodymyr Prykhodko <diegomackenzie24@gmail.com>
2025-01-26 21:10:34 +0200

    deploy fix 1;

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/landing/",
   output: "export",
+  assetPrefix: "/landing",
   images: {
     remotePatterns: [
       {
