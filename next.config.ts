31272280245c3f60d830d3f7b1b1ed4136d3ccf8
Volodymyr Prykhodko <diegomackenzie24@gmail.com>
2025-01-27 21:07:17 +0200

    fixed images issue;

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "",
+  basePath: "/landing", // Replace "landing" with your repository name
+  assetPrefix: "/landing/", // Same as basePath
   output: "export",
   images: { unoptimized: true },
 };
