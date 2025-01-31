73fa2fef10cdc375ce345335e137ea41b0fe5f23
Volodymyr Prykhodko <diegomackenzie24@gmail.com>
2025-01-31 15:36:27 +0200

    added success page;

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Success = () => {
-  return <div>Form has been submitted!</div>;
+  return (
+    <section className="flex items-center justify-center flex-col h-[100vh]">
+      <h1 className="text-bold text-blue text-xl lg:text-8xl mb-4">
+        Form has been submitted!
+      </h1>
+      <p className="text-lg text-blue">We will get back to you shortly</p>
+    </section>
+  );
 };
 
 export default Success;
