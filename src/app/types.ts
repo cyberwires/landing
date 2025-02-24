dfcfe077cea508c0fca6bebf6f88b380d0eb26f5
InnaShp <107990414+InnaShp@users.noreply.github.com>
2025-02-24 12:44:43 +0200

    Fix types error

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,7 +2,7 @@ import { StaticImageData } from "next/image";
 
 export interface ProjectData {
   name: string;
-  description: string;
+  description: string[];
   stack: string[];
   link: string;
   logo: StaticImageData;
