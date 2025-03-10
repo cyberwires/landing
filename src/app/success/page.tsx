82255f02e1dab597266509841fe1ade9c08d2ed3
Inna Shpachynska <inna.shpachynska@gmail.com>
2025-03-10 16:00:27 +0000

    Add link "go back"

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,12 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const Success = () => {
   return (
-    <section className="flex items-center justify-center flex-col h-[100vh]">
-      <h1 className="text-bold text-blue text-xl lg:text-8xl mb-4">
-        Form has been submitted!
+    <section className="flex items-center justify-center flex-col h-screen text-blue text-center px-6">
+      <h1 className="text-3xl md:text-6xl lg:text-8xl mb-4">
+        Form Submitted!
       </h1>
-      <p className="text-lg text-blue">We will get back to you shortly</p>
+      <p className="text-lg md:text-xl opacity-90">
+        We will get back to you shortly.
+      </p>
+      <Link
+        href="/"
+        className="mt-6 font-semibold text-lg hover:underline"
+      >
+        Go Back
+      </Link>
     </section>
   );
 };
